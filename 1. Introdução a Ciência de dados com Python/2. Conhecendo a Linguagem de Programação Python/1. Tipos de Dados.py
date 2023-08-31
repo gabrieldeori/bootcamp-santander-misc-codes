906318754c4a2c5619a69b5bc55e60178cb97aba
gabrieldeori <deori.pessoal@gmail.com>
2023-08-31 16:07:57 -0300

    feat: Testando tipos de dados no Python

---
+++ 1. Introdução a Ciência de dados com Python/2. Conhecendo a Linguagem de Programação Python/1. Tipos de Dados.py
@@ -0,0 +1,22 @@
+print() # Esse pula linha vazio
+print("Print String") # classe str() (Construtor de tipo)
+print()
+
+print("int:") # classe int() (Construtor de tipo)
+print(1+10)
+print(1+100+1000)
+print()
+
+print("float:") # classe float() (Construtor de tipo)
+print(10.51251+20)
+print(1.3+200.51+1000.0)
+print()
+
+print("bool:") # classe bool() (Construtor de tipo)
+print(True)
+print(False)
+print()
+
+# print(1 + "Teste")
+# Executa até aqui e dá erro:
+# TypeError: unsupported operand type(s) for +: 'int' and 'str'
