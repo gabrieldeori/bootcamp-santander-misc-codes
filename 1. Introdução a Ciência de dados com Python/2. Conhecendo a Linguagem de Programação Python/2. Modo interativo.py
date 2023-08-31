53a3995c2c5e4b46e3d77ee909dfeac80052b4a8
gabrieldeori <deori.pessoal@gmail.com>
2023-08-31 17:22:23 -0300

    feat: Estudando modo interativo Python + docs

---
+++ 1. Introdução a Ciência de dados com Python/2. Conhecendo a Linguagem de Programação Python/2. Modo interativo.py
@@ -0,0 +1,6 @@
+dir() # retorna lista de nomes no escopo local atual.
+dir(100) # Com argumento retorna lista de atributos válidos para o objeto.
+help() # Invoca o sistema de ajuda interativa do Python.
+help(100) # Invoca o sistema de ajuda interativa do Python para o objeto 100.
+help(print) # Invoca o sistema de ajuda interativa do Python para o objeto print.
+help(print.__add__) # Invoca o sistema de ajuda interativa do Python para o método add do objeto print.
