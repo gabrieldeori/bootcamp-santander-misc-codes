6113feb1fa00d4a6700a9621a8553dc8fa4bd468
gabrieldeori <deori.pessoal@gmail.com>
2023-08-31 19:20:57 -0300

    feat: Treinando operadores de atribuição

---
+++ 1_introdução_a_ciência_de_dados_com_python/3_tipos_de_operadores_com_python/3_operadores_de_atribuicao.py
@@ -0,0 +1,45 @@
+# São operadores utilizados para definir o valor inicial ou sobrescrever o valor de uma variável
+
+# | Simples | = |
+# | --- | --- |
+# | Adição | += |
+# | Subtração | -= |
+# | Multiplicação | *= |
+# | Divisão | /= |
+# | Divisão inteira | //= |
+# | Módulo | % |
+# | Exponenciação | ** |
+
+# Exemplifique
+
+print("Simples")
+x = 5
+print(x)
+
+print("Adição")
+x += 1
+print(x)
+
+print("Subtração")
+x -= 1
+print(x)
+
+print("Multiplicação")
+x *= 2
+print(x)
+
+print("Divisão")
+x /= 2
+print(x)
+
+print("Divisão inteira")
+x //= 2
+print(x)
+
+print("Exponenciação")
+x **= 2
+print(x)
+
+print("Módulo")
+x %= 3
+print(x)
