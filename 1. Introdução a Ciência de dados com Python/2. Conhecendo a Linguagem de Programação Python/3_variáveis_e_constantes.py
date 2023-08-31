600ee9cb9dab61af6777ca4cb6dd5c95c9018231
gabrieldeori <deori.pessoal@gmail.com>
2023-08-31 17:44:22 -0300

    feat: Treinando variáveis e constantes

---
+++ 1. Introdução a Ciência de dados com Python/2. Conhecendo a Linguagem de Programação Python/3_variáveis_e_constantes.py
@@ -0,0 +1,18 @@
+user_age = 23
+USER_NAME = "Gabriel" # Não é uma constante de fato, é uma convenção de código.
+# age, name = (23, "Deori")
+# age, name = 23, "Deori"
+print(f'Meu nome é {USER_NAME} e eu tenho {user_age} anos de idade')
+
+user_age = 28
+print(f'Não! Meu nome é {USER_NAME} e eu tenho {user_age} anos de idade')
+
+# Put all states of Brazil in a list
+SOME_STATES_BRAZIL = [
+  "Minas Gerais",
+  "Bahia",
+  "Pernambuco",
+  "Ceará",
+  "São Paulo",
+  "Rio de Janeiro",
+]
