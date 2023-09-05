f80e2b9bd023532a5b457e56d7b29acab8923e71
gabrieldeori <deori.pessoal@gmail.com>
2023-09-05 17:06:21 -0300

    feat: Treinando métodos em str

---
+++ 1_introdução_a_ciência_de_dados_com_python/5_manipulando_strings_com_python/1_conhecendo_metodos_uteis_da_classe_string.py
@@ -0,0 +1,49 @@
+# Conhecendo métodos úteis da classe string
+print("curso = 'pYtHon'")
+curso = 'pYtHon'
+
+print("print(curso.upper())")
+print(f"# {curso.upper()}")
+print("print(curso.lower())")
+print(f"# {curso.lower()}")
+print("print(curso.capitalize())")
+print(f"# {curso.capitalize()}")
+print("print(curso.title())")
+print(f"# {curso.title()}")
+
+print("print(curso.strip())")
+print(f"# {curso.strip()}")
+print("print(curso.lstrip())")
+print(f"# {curso.lstrip()}")
+print("print(curso.rstrip())")
+print(f"# {curso.rstrip()}")
+
+print("print(curso.replace('p', 'P'))")
+print(f"# {curso.replace('p', 'P')}")
+print("print(curso.replace('p', 'P', 1))")
+print(f"# {curso.replace('p', 'P', 1)}")
+
+print("print(curso.split('t'))")
+print(f"# {curso.split('t')}")
+print("print(curso.split('t', 1))")
+print(f"# {curso.split('t', 1)}")
+
+print("print(curso.center(10, '#'))")
+print(f"# {curso.center(10, '#')}")
+print("print(curso.ljust(10, '#'))")
+print(f"# {curso.ljust(10, '#')}")
+print("print(curso.rjust(10, '#'))")
+print(f"# {curso.rjust(10, '#')}")
+
+print("'.'.join(curso)")
+print('.'.join(curso))
+
+print("print(curso.count('t'))")
+print(f"# {curso.count('t')}")
+print("print(curso.count('T'))")
+print(f"# {curso.count('T')}")
+
+print("print(curso.find('t'))")
+print(f"# {curso.find('t')}")
+print("print(curso.find('T'))")
+print(f"# {curso.find('T')}")
